be42751bef9c2993bbb3d8f33bb9bb6460b31622
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-01-18 22:33:57 +0100

    updated contacts

--- src/public/contacts.php
+++ src/public/contacts.php
@@ -2,8 +2,8 @@
 $pageTitle = 'Contatti';
 
 $contacts = [
-    ['name' => 'Alessandro Rebosio', 'role' => 'Developer', 'email' => 'email.com'],
-    ['name' => 'Grazia Bochdanovits de Kavna', 'role' => 'Developer', 'email' => 'email.com']
+    ['name' => 'Alessandro Rebosio', 'role' => 'Developer', 'email' => 'alessandro.rebosio@studio.unibo.it'],
+    ['name' => 'Grazia Bochdanovits de Kavna', 'role' => 'Developer', 'email' => 'grazia.bochdanovits@studio.unibo.it']
 ];
 
 $iconPerson = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person text-primary" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" /></svg>';
@@ -37,5 +37,6 @@ ob_start();
     <?php endforeach; ?>
 </div>
 <?php
+
 $content = ob_get_clean();
 include 'template/base.php';
