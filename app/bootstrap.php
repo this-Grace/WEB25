2c8e03b69a49207308da426be0c97d9d9fc573b1
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-01-23 12:32:38 +0100

    added event in bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,10 +7,12 @@
  * It should be included in other PHP files that need database access.
  */
 require_once __DIR__ . '/database.php';
-require_once __DIR__ . '/orm/User.php';
 require_once __DIR__ . '/orm/Category.php';
+require_once __DIR__ . '/orm/Event.php';
+require_once __DIR__ . '/orm/User.php';
 
 $dbh = new DatabaseHelper("localhost", "root", "", "web25", 3306, 'utf8mb4');
 
 $userMapper = new User($dbh->getConnection());
+$eventMapper = new Event($dbh->getConnection());
 $catMapper = new Category($dbh->getConnection());
